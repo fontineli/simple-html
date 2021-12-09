a78bca1196bf4d043fc9da219e7ed45e28bf73a0
Herton <d@ntas.cc>
2021-12-08 23:32:08 -0500

    Playing with binding data to persist

--- scripts.js
+++ scripts.js
@@ -12,6 +12,43 @@ window.onload = () => {
 customElements.define('current-year', class CurrentYear extends HTMLElement {
     constructor() {
         super();
-        this.textContent = (new Date()).getFullYear();
+        this.textContent = new Date().getFullYear();
     }
 });
+
+customElements.define('time-spent', class TimeSpent extends HTMLElement {
+    constructor() {
+        super();
+        const self = this;
+        self.textContent = parseInt(localStorage.getItem('timeSpent'));
+        setInterval(function() {
+            self.textContent = parseInt(self.textContent) + 1;
+            localStorage.setItem('timeSpent', self.textContent);
+        }, 1000);
+    }
+});
+
+
+customElements.define('render-content', class RenderContent extends HTMLElement {
+    constructor() {
+        super();
+        const self = this;
+        const model = self.dataset ? self.dataset.model : '';
+        this.textContent = localStorage.getItem(model);
+        setInterval(function() {
+            self.textContent = localStorage.getItem(model);
+        }, 100);
+    }
+});
+
+customElements.define('storage-input', class StorageInput extends HTMLInputElement {
+    constructor() {
+        super();
+        const self = this;
+        const model = self.name;
+        self.value = localStorage.getItem(model) || '';
+        setInterval(function() {
+            localStorage.setItem(model, self.value);
+        }, 100);
+    }
+}, { extends: 'input' });
